dc0c60f5c4344bc2f832b06f8b1f40e63584ed02
Johnson Chinonso <johsonchinonso19@gmail.com>
2017-05-16 10:14:37 +0100

    refactored route

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', (req, res) => {
+  res.render('index.ejs', { title: 'Express' });
+});
+
+module.exports = router;
