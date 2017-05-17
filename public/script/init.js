2a4b79a9d81f0cfad9e3908b52de7293e063a92a
Johnson Chinonso <johsonchinonso19@gmail.com>
2017-05-17 13:48:25 +0100

    static files

--- public/script/init.js
+++ public/script/init.js
@@ -1,11 +1,6 @@
 $(document).ready(function() {
-<<<<<<< HEAD
-  $('.carousel.carousel-slider').carousel({fullWidth: true});
-  $(".button-collapse").sideNav();
-});
-=======
   $('.carousel.carousel-slider').carousel({ fullWidth: true });
   $('.button-collapse').sideNav();
   $('.slider').slider();
 });
->>>>>>> 4b169f46a68601be143fa17a53a8ab6149ed4313
+
