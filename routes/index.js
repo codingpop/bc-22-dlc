57b78df612d9c114131ae5ad810cb0f42b5ff5f2
Johnson Chinonso <johsonchinonso19@gmail.com>
2017-05-17 13:49:23 +0100

    included dashboard routes

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,8 @@ router.get('/', (req, res) => {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/dashboard', (req, res) => {
+  res.render('dashboard', {});
+});
+
 module.exports = router;
