b34e1d61ecbe98cd2ee1334b378d85dfd39dbb0d
Johnson Chinonso <johsonchinonso19@gmail.com>
2017-05-18 03:05:56 +0100

    refactored the search route

--- routes/signin.js
+++ routes/signin.js
@@ -1,50 +1,19 @@
 import express from 'express';
 import passport from 'passport';
-import passportLocal from 'passport-local';
-import User from '../models/user';
 
+const app = express();
 
-const localStrategy = passportLocal.Strategy;
-const router = express.Router();
-
-
-router.get('/', (req, res) => {
-  res.render('login', { title: 'Log in' });
+app.get('/', (req, res) => {
+  res.render('login');
 });
 
-passport.serializeUser((user, done) => {
-  done(null, user._id)
-});
+app.post('/',
+  passport.authenticate('local', { successRedirect: '/', failureRedirect: '/login' }));
 
-passport.deserializeUser((id, done) => {
-  User.getUserById(id, (err, user) =>{
-    done(err, user);
-  });
-});
+// app.get('/logout', (req, res) => {
+//   req.logout();
+//   res.redirect('/');
+// });
 
-router.post('/', passport.authenticate('local', {failureRedirect: '/', failureFlash: 'Wrong User name and password'}, (req, res) => {
-  req.flash('success', 'You are now Log in');
-  res.redirect('/');
-}));
 
-passport.use(new localStrategy(
-  (email, password, done) => {
-    User.getUserByEmail(email, (err, user) => {
-      if (err) throw err;
-      if (!user) {
-        return done(null, false, {message: 'Unknown User'});
-      }
-      User.comparePassword(password, user.password, (err, isMatch) => {
-        if (err) return done(err); 
-        if (isMatch) {
-          return done(null, user);
-        } else {
-          console.log('Invalid Password');
-          return done(null, false, { message: 'Invalid Password' });
-        }
-      });
-    });
-  }
-));
-
-module.exports = router;
+module.exports = app;
